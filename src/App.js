eb082e8ae5aee3fd309c7ff528d6171de79280f7
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-07-02 23:58:20 +0100

    add new routes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import PatientsPersonalPage from './doctor/pages/PatientsPersonalPage';
 import DrugsTestDiagnosis from './doctor/pages/DrugsTestDiagnosis';
 import PatientHistory from './doctor/pages/PatientHistory';
 import HistoryOverview from './doctor/pages/HistoryOverview';
+import CashierHome from './cashier/pages/CashierHome';
+import PatientInvoice from './cashier/pages/PatientInvoice';
+import PharmacistHome from './pharmacist/pages/PharmacistHome';
+import PharmacistInvoice from './pharmacist/pages/PharmacistInvoice';
+import LabHome from './lab/pages/LabHome';
+import LabInvoice from './lab/pages/LabInvoice';
 
 function App() {
   const theme = createTheme({
@@ -27,12 +33,28 @@ function App() {
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/*" element={<AdminNav />} />
+
+            {/* receptionist route */}
             <Route path="/receptionist" element={<ReceptionistHome />} />
+
+            {/* doctor routes */}
             <Route path="/doctor" element={<DoctorHome />} />
             <Route path="/patient/:id/:name" element={<PatientsPersonalPage />} />
             <Route path="/diagnosis-drugs-test" element={<DrugsTestDiagnosis />} />
             <Route path="/history-overview" element={<HistoryOverview />} />
             <Route path="/history" element={<PatientHistory />} />
+
+            {/* cashier route */}
+            <Route path="/cashier" element={<CashierHome />} />
+            <Route path="/patient-invoice" element={<PatientInvoice />} />
+
+            {/* pharmacist routes */}
+            <Route path="/pharmacist" element={<PharmacistHome />} />
+            <Route path="/approved-invoice" element={<PharmacistInvoice />} />
+
+            {/* lab routes */}
+            <Route path="/lab" element={<LabHome />} />
+            <Route path="/lab-invoice" element={<LabInvoice />} />
           </Routes>
         </BrowserRouter>
       </div>
