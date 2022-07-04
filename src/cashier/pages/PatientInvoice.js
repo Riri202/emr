4718b444beea80019a6347cbf0ab45b4802b0903
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-07-04 23:21:53 +0100

    import approve-payment-btn

--- src/cashier/pages/PatientInvoice.js
+++ src/cashier/pages/PatientInvoice.js
@@ -15,6 +15,7 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Button from '@mui/material/Button';
 import DropdownButton from '../../common-components/DropdownButton';
+import ApprovePaymentBtn from '../components/ApprovePaymentBtn';
 
 const useStyles = makeStyles({
   table: {
@@ -25,10 +26,6 @@ function PatientInvoice() {
   const classes = useStyles();
   const headers = ['Item description', 'Qty', 'Rate', 'Amount'];
 
-  // state for changing button color when payment is approved
-  const [isPaymentApproved, setIsPaymentApproved] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-
   // get drugs list
   const drugs = JSON.parse(localStorage.getItem('drugsList'));
   // get drugs total amount
@@ -36,13 +33,6 @@ function PatientInvoice() {
     .map((drug) => drug.quantity * drug.unitPrice)
     .reduce((prev, curr) => prev + curr, 0);
 
-  const handleApprovePayment = () => {
-    setIsLoading(true);
-    setTimeout(() => {
-      setIsPaymentApproved(true);
-      setIsLoading(false);
-    }, 6000);
-  };
   return (
     <>
       <Nav />
@@ -110,52 +100,7 @@ function PatientInvoice() {
               </Table>
             </TableContainer>
           </Paper>
-          <div>
-            {!isPaymentApproved ? (
-              <Box sx={{ position: 'relative' }}>
-                <Button
-                  disabled={isLoading}
-                  onClick={handleApprovePayment}
-                  className="p-2 mt-1 bg-green-500 text-[#000] ml-3">
-                  Approve payment of&nbsp;
-                  <span> &#8358;</span>
-                  {drugsTotalAmount}
-                </Button>
-                {isLoading && (
-                  <CircularProgress
-                    color="success"
-                    size={24}
-                    sx={{
-                      position: 'absolute',
-                      top: '50%',
-                      left: '50%',
-                      zIndex: 1,
-                      marginTop: '-12px',
-                      marginLeft: '-12px'
-                    }}
-                  />
-                )}
-              </Box>
-            ) : (
-              <>
-                <Grow in={isPaymentApproved}>
-                  <div className="flex flex-col">
-                    <Chip
-                      label="Payment has been approved by Cashier Rose"
-                      icon={<CheckCircleIcon />}
-                      color="success"
-                    />
-                    <Box className="flex justify-end mt-3 bg-white p-1">
-                      <DropdownButton
-                        btnText="send to"
-                        menuItems={['dr. Stark', 'Dr Drake Remurray']}
-                      />
-                    </Box>
-                  </div>
-                </Grow>
-              </>
-            )}
-          </div>
+          <ApprovePaymentBtn amount={drugsTotalAmount} />
         </section>
       </div>
     </>
