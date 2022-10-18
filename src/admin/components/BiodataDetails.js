52da3ef587e56921613b0a9504ed13af6a4217d9
Rita-Maria Oladokun <Rita-Maria@MacBook-Pro.local>
2022-10-18 23:42:45 +0100

    add registration and occupation fields

--- src/admin/components/BiodataDetails.js
+++ src/admin/components/BiodataDetails.js
@@ -4,7 +4,7 @@ import FolderSharedIcon from '@mui/icons-material/FolderShared';
 
 // eslint-disable-next-line react/prop-types
 function BiodataDetails({ info }) {
-  const arr = ['age', 'sex', 'genoType', 'address', 'bloodGroup'];
+  const arr = ['age', 'sex', 'genoType', 'address', 'bloodGroup', 'registration', 'occupation'];
   return (
     <List component="li" disablePadding className="grid grid-cols-2 gap-4">
       {arr.map((item, index) => {
